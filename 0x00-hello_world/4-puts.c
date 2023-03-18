766b28d72ca6f916c47c85c79540baeecc8de868
Myctees <mickey4thereds@yahoo.com>
2023-03-18 02:28:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - writing a program that prints a line using using the printf function
+ * Return: 0 (is a success)
+ */
+
+int main(void)
+{
+put"/ "Programming is like building a multilingual puzzle");
+return (0);
+}
