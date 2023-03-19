d62866b60c4552521e851001d195545fbc81a0d7
Myctees <mickey4thereds@yahoo.com>
2023-03-18 22:12:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with function
+ * main - A program that print a line using the printf function
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
